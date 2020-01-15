a4f41a03969b6a041a73c3a5740c336f4fa2d59f
Mateus Moura Santos <msantos.96@outlook.com>
2020-01-14 23:06:29 -0300

    Initial Commit

---
+++ simplefinance/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  simplefinance
+//
+//  Created by Mateus on 14/01/20.
+//  Copyright © 2020 Mateus. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
